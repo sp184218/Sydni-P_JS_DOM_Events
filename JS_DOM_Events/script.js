c839096463589e9b3497290bb680c89518a94a7c
sp184218 <sp184218@gmail.com>
2025-04-01 18:54:10 -0700

    Added DOMContentLoaded, KeyDown, and Click events

---
+++ JS_DOM_Events/script.js
@@ -0,0 +1,23 @@
+//When the dom is fully loaded, this will be fired
+document.addEventListener("DOMContentLoaded", function(){
+
+    //Get the element with an id of onload
+    let onLoadEl = document.getElementById("onload");
+   
+    //Add some HTML within the element we grabbed
+    onLoadEl.innerHTML = "<h1> I loaded cause the DOM was fully loaded</h1>"
+   
+   })
+
+   //Get the element that will show our key code
+let directionEl = document.getElementById("direction");
+
+// Can be changed with Key press or key up depending on what you might need
+document.addEventListener("keydown", function(e) {
+
+ // When a key is pressed, the element with the id of direction will show the message
+ directionEl.innerText = `The Key you pressed is ${e.code}`;
+
+});
+
+   
