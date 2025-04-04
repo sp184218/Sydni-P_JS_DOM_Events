520426219f2d54ca7bff1b4c8890f54a860dddd7
sp184218 <sp184218@gmail.com>
2025-04-04 11:08:11 -0700

    Added missing "You clicked me!" text update on button click and ensured proper toggle functionality.

--- JS_DOM_Events/script.js
+++ JS_DOM_Events/script.js
@@ -1,23 +1,27 @@
-//When the dom is fully loaded, this will be fired
-document.addEventListener("DOMContentLoaded", function(){
-
-    //Get the element with an id of onload
+// When the DOM is fully loaded, this will be fired
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the element with an id of onload
     let onLoadEl = document.getElementById("onload");
-   
-    //Add some HTML within the element we grabbed
-    onLoadEl.innerHTML = "<h1> I loaded cause the DOM was fully loaded</h1>"
-   
-   })
-//Get the element that will show our key code
+    // Add some HTML within the element we grabbed
+    onLoadEl.innerHTML = "<h1> I loaded cause the DOM was fully loaded</h1>";
+
+    // Get the button by its ID
+    let clickMeButton = document.getElementById("clickMe");
+    // Add event listener to change the text when clicked
+    clickMeButton.addEventListener("click", function() {
+        clickMeButton.innerText = "You clicked me!";
+    });
+});
+
+// Get the element that will show our key code
 let directionEl = document.getElementById("direction");
 
 // Can be changed with Key press or key up depending on what you might need
 document.addEventListener("keydown", function(e) {
-
- // When a key is pressed, the element with the id of direction will show the message
- directionEl.innerText = `The Key you pressed is ${e.code}`;
-
+    // When a key is pressed, the element with the id of direction will show the message
+    directionEl.innerText = `The Key you pressed is ${e.code}`;
 });
+
 // Function to generate a random hex color
 function getRandomColor() {
     let letters = "0123456789ABCDEF";
@@ -33,4 +37,8 @@ document.getElementById("randomColorBtn").addEventListener("click", function() {
     document.body.style.backgroundColor = getRandomColor();
 });
 
-   
+// Function to toggle button glow effect
+function changeBtnStyle() {
+    const btn = document.querySelector(".cool-btn");
+    btn.classList.toggle("glow"); // Toggles the glow effect
+}
